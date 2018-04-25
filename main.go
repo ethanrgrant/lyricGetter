c66cb1b6d7272025faf1ba724bbc1b68e8c8d140
Ethan Grant <ethan.r.grant@gmail.com>
2018-04-24 22:04:14 -0400

    get song names in list instead of one blob, set up reading of song data in multithreaded fashion

--- main.go
+++ main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 )
 
-func constructUrl(artist string) (string)  {
-    return "https://genius.com/artists/" + artist
+func constructUrl(artist string) string {
+	return "https://genius.com/artists/" + artist
+}
+
+func processArtist(songNum int, finishCh chan bool, songLyricCh chan SongLyrics) {
+    totalSeen := 0
+    for {
+        select {
+        case songLyric := <-songLyricCh:
+            fmt.Printf("\n%s \n %s", songLyric.Song, songLyric.Lyrics)
+        case <-finishCh:
+            // if all songs have been processed move on
+            totalSeen++
+            if totalSeen >= songNum {
+                return
+            }
+        }
+    }
 }
 
 func main() {
 
-    if len(os.Args) == 1 {
-        fmt.Println("please include one artists name as cmd line args")
-    }
-    
-    
-    artists := os.Args[1:]
-   /* foundUrls := make([]string, 0)
-    urlCh := make(chan string)
-    finishCh := make(chan bool) */
-    for _, artist := range artists {
-        GetSongList(artist)
-        /*
-        cleanUrl := constructUrl(artist)
-        fmt.Println(cleanUrl)
-        go GetUrls(cleanUrl, urlCh, finishCh) */
-    }
-/*
-    for artistIdx := 0; artistIdx < len(artists); {
-        select {
-        case url := <-urlCh:
-            foundUrls = append(foundUrls, url) 
-        case <- finishCh:
-            artistIdx++
-        }
-    }
+	if len(os.Args) == 1 {
+		fmt.Println("please include one artists name as cmd line args")
+	}
 
-    for _, url := range foundUrls {
-        fmt.Printf("url: %s", url)
-    }*/
+	artists := os.Args[1:]
+	songLyricCh := make(chan SongLyrics)
+	finishCh := make(chan bool)
+
+	for _, artist := range artists {
+        songNum := 0
+		GetSongList(artist, &songNum, songLyricCh, finishCh)
+        processArtist(songNum, finishCh, songLyricCh)
+	}
 }
--- scraper.go
+++ scraper.go
@@ -1,91 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "golang.org/x/net/html"
-    "strings"
+	"fmt"
+    //"strings"
 
-    // import third party libraries
-    "github.com/PuerkitoBio/goquery"
+	// import third party libraries
+	"github.com/PuerkitoBio/goquery"
 )
 
-type AristSongLyrics struct {
-    Artist string
-    Lyrics string
-    Song string
+type SongLyrics struct {
+	Lyrics string
+	Song   string
 }
 
-// Helper function to pull the href attribute from a Token
-func getHref(t html.Token) (ok bool, href string) {
-	// Iterate over all of the Token's attributes until we find an "href"
-	for _, a := range t.Attr {
-		if a.Key == "href" {
-			href = a.Val
-			ok = true
-		}
-	}
-	
-	return
+func getLyrics(artistName string, songName string, songLyricCh chan SongLyrics, finishCh chan bool) {
+	songLyricCh <- SongLyrics{Song: songName, Lyrics: "These are lyrics"}
+	finishCh <- true
 }
 
-// Gets all urls associated with the given url
-func GetUrls(url string, urlCh chan string, doneSignalCh chan bool) {
-    resp, err := http.Get(url)
-    
-    defer func() {
-        doneSignalCh <- true
-    }()
-    if err != nil {
-		fmt.Println("ERROR: Failed to crawl \"" + url + "\"")
+func GetSongList(artistName string, songNum * int, songLyricCh chan SongLyrics, finishCh chan bool) {
+	finalUrl := "http://www.songfacts.com/artist-" + artistName + ".php"
+	doc, err := goquery.NewDocument(finalUrl)
+	if err != nil {
+		fmt.Println("ERROR: failed to get songs for " + artistName + "!")
 		return
-    }
-    defer resp.Body.Close()
+	}
 
-    tokenizer := html.NewTokenizer(resp.Body)
-    for {
-        token := tokenizer.Next()
+    *songNum = doc.Find(".songullist-orange ").Find("a").Each(func(i int, s *goquery.Selection) {}).Length()
 
-        switch {
-        case token == html.ErrorToken:
-            return
-        case token == html.StartTagToken:
-            tok := tokenizer.Token()
-            //fmt.Println(tok)
-
-            if tok.Data == "a" {
-                continue
-            }
-            
-            ok, url := getHref(tok)
-            if !ok {
-                continue
-            }
-
-            if strings.Index(url, "http") == 0 {
-                urlCh <- url
-            }
-        }
-    }
-}
-
-
-func GetSongList(artistName string) {
-    finalUrl := "http://www.songfacts.com/artist-" + artistName + ".php"
-
-/*
-    artistSong := Artist{Artist: artistName
-    defer func() {
-       outArtistSongChan <-  artistSong
-    }*/
-    doc, err := goquery.NewDocument(finalUrl)
-    if err != nil {
-        fmt.Println("ERROR: failed to get songs for " + artistName + "!")
-        return
-    }
-    doc.Find(".songullist-orange").Each(func(i int, s *goquery.Selection) {
-    // For each item found, get the band and title
-        band := s.Find("a").Text()
-        fmt.Printf("%s: %s\n", i, band)
-    })
+	doc.Find(".songullist-orange ").Find("a").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the lyrics
+		go getLyrics(artistName, s.Text(), songLyricCh, finishCh)
+	})
 }
